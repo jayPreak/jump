4d232a2e4e6e1a20174431d3d72b3f41c1280725
jaypreak <jayesh.preak2003@gmail.com>
2024-10-01 19:30:30 +0530

    yum

--- player.gd
+++ player.gd
@@ -6,3 +6,24 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	# We create a local variable to store the input direction.
+	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
