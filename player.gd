772acca2d84078ce0e4ce5dcaaccf97039e9e78b
jaypreak <jayesh.preak2003@gmail.com>
2024-10-01 19:04:15 +0530

    yuh

---
+++ player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
