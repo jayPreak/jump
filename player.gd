c6f01c1c157815e82815dcf64e1a35722df8c5a5
jaypreak <jayesh.preak2003@gmail.com>
2024-10-03 21:35:38 +0530

    sex

--- player.gd
+++ player.gd
@@ -27,3 +27,14 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
