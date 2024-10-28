23e4ce904ccdeb31a144fec2f1ab1f68d9b9037d
jaypreak <jayesh.preak2003@gmail.com>
2024-10-28 20:29:46 +0530

    meowwwwsaa

---
+++ mob.gd
@@ -0,0 +1,10 @@
+extends CharacterBody3D
+
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 10
+# Maximum speed of the mob in meters per second.
+@export var max_speed = 18
+
+
+func _physics_process(_delta):
+	move_and_slide()
