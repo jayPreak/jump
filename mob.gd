7d3e3a7d8571ff4ca8e68ceeab796850306a7e15
jaypreak <jayesh.preak2003@gmail.com>
2024-10-28 20:30:44 +0530

    Refactor mob initialization and movement logic

--- mob.gd
+++ mob.gd
@@ -5,6 +5,21 @@ extends CharacterBody3D
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
+# This function will be called from the Main scene.
+func initialize(start_position, player_position):
+	# We position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player.
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotate this mob randomly within range of -45 and +45 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 func _physics_process(_delta):
 	move_and_slide()
