fabed10e8c0233d5f71c4d79d9fd8d467ca11c53
jaypreak <jayesh.preak2003@gmail.com>
2024-10-30 22:21:19 +0530

    meow

--- main.gd
+++ main.gd
@@ -2,15 +2,6 @@ extends Node
 
 @export var mob_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
