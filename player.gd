9eadf20d91b843ac01b329859b1c5f79d0214ccd
jaypreak <jayesh.preak2003@gmail.com>
2024-11-04 22:45:45 +0530

    ewww

--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ extends CharacterBody3D
 
 @export var jump_impulse = 20
 
+@export var bounce_impulse = 16
+
 var target_velocity = Vector3.ZERO
 
 
@@ -40,3 +42,22 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+	
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
